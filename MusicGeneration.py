b19d4caac998f654f1efa4585b11835a232832d4
pycsham <shampuiyanchloe@gmail.com>
2020-04-15 06:42:08 -0400

    Created a chords track (playing the backing chords). Implemented the grouping of terminal sequence into bars

--- MusicGeneration.py
+++ MusicGeneration.py
@@ -2,68 +2,31 @@ from random import choice
 import music21
 
 class BluesMusicGeneration:
-    # chord sequence is currently default to the classical blues chord sequence and the E major scale
-    def __init__(self, sequence, key="E", chordSequence=[1,1,1,1,4,4,1,1,5,5,1,1]):
+    # chord sequence is currently default to the classical blues chord sequence and the C major scale
+    # This version only uses the rhythmic structure of the CFG and overlays the blues scale on top of it
+    def __init__(self, sequence, key="C", chordSequence=[1,1,1,1,4,4,1,1,5,5,1,1]):
         self.sequence = sequence # the sequence produced by the CFG 
         self.key = key # a string that represents which key we're working with 
         self.chordSequence = chordSequence # the chord sequence, where each element in the list represents chord per bar
         self.result = music21.stream.Stream() # the result stream of notes
-        self.octave = ["3","4","5","6"] 
+        self.chordsTrack = music21.stream.Stream() # Stores the chords being played as the "Left hand side"
+        self.barSequence = []
+        
+        # self.chordsMap = {
+        #     1: 'C3 E3 G3 A#3',
+        #     4: 'C3 D#3 F3 A3',
+        #     5: 'D3 F3 G3 B3'
+        # }
 
-        # The following are dictionaries of the form: int -> set(str) where the key represents a chord, and the set of string represents a set of notes
-
-        # Chords: taking the dominant 7th chords
-        self.chordTone = {
-            1: ["E", "G#", "B", "D"],
-            4: ["A", "C#", "E", "G"],
-            5: ["B", "D#", "F#", "A"]
-        }
-        # The 9th, 11th and 13th notes of each chord (extended jazz chords)
-        self.colorTone = {
-            1: ["F#", "A", "C#"],
-            4: ["B", "D", "F#"],
-            5: ["C#", "E", "A#"]
-        }
-
-        # Taking the Diatonic-Chromatic Enclosure approach
-        # (diatonic above, chromatic below)
-        # Note: these are not necessarily scale tones since it's chromatic below
-        self.approachTone = {
-            1: {
-                "E": ["D#", "F#"],
-                "G#": ["G", "A"],
-                "B": ["A#", "C#"],
-                "D": ["C#", "E"],
-                "F#": ["F", "G#"],
-                "A": ["G#", "B"],
-                "C#": ["C", "D#"]
-            },
-            4: {
-                "A": ["G#", "B"],
-                "C#":["C", "D"],
-                "E":["D#", "F#"],
-                "G":["F#", "A"],
-                "B":["A#", "C#"],
-                "D":["C#", "E"],
-                "F#":["F", "G#"]
-            },
-            5: {
-                "B":["A#", ""],
-                "D#":["D", "E"],
-                "F#":["F", "G#"],
-                "A":["G#", "B"],
-                "C#":["C", "D#"],
-                "E": ["D#", "F#"],
-                "A#": ["A", "B"]
-            }
-        }
-        # Taking the major blues scale since we're assuming the underlying chords are major 7th
-        # The major blues scale: R-2-b3-3-5-6 (not too sure about this, it's supposed to be soronous to the current chord?)
-        self.scaleTone = {
-            1: ["E", "F#", "G", "G#", "B", "C#"],
-            4: ["A", "B", "C", "C#", "E", "F#"],
-            5: ["B", "C#", "D", "D#", "F#", "G#"]
-        } 
+        # for c in self.chordSequence:
+        #     chordNotes = self.chordsMap[c]
+        #     for i in range(3):
+        #         duration = music21.duration.Duration(2.0)
+        #         chord = music21.chord.Chord(chordNotes, duration=duration)
+        #         self.chordsTrack.append(chord)
+        
+        # fp = self.chordsTrack.write('midi', fp="./results/chordsTrack.midi")
+       
 
         # Mapping the rhythm to duration, using the convention in music21
         self.rhythmMap = {
@@ -72,102 +35,59 @@ class BluesMusicGeneration:
             "4": 1,
             "4.": 1.5,
             "8": 0.5,
-            "16": 0.25,
-            "4/3": 2.0/3.0,
-            "8/3": 1.0/3.0,
-            "16/3": 1.0/6.0
+            "16": 0.25
         }
 
 
-        # Mapping each note in the sequence from a string to a tuple (rhythm, pitch) 
+        # Mapping each note in the sequence from a string to a tuple (pitch, rhythm)
         for index,note in enumerate(sequence):
             self.sequence[index] = (note[0], note[1:])
 
+        # Categorize the sequence into bars according to rhythm
+        curBeats = 0
+        bar = []
+        index = 0
+        while index < len(self.sequence):
+            s = self.sequence[index]
+            r = s[1]
+            #  New Bar
+            if curBeats >= 4: #There could be carry over from the previous bar
+                curBeats = curBeats - 4
+                self.barSequence.append(bar)
+                bar = []
+
+            if r == "4/3" or r == "8/3" or r == "16/3": # Consider a group of triplets, assume these don't stride across bars
+                if r[0] == "4":
+                    curBeats += 2
+                elif r[0] == "8":
+                    curBeats += 1
+                else:
+                    curBeats += 0.5
+                for i in range(3):
+                    bar.append(self.sequence[index+i])
+                index += 3
+            else:
+                bar.append(self.sequence[index])
+                index += 1
+                curBeats += self.rhythmMap[r]
+        if bar:
+            self.barSequence.append(bar)
 
 
-
-    # Generates a chord tone , color tone, or scale tone note
-    def generateChordColorScaleNote(self, chord, tone, duration):
-        pitch = ""
-        if tone == "C":
-            pitch = choice(self.chordTone[chord])
-        elif tone == "L":
-            pitch = choice(self.colorTone[chord])
-        elif tone == "S":
-            pitch = choice(self.scaleTone[chord])
-        
-        octave = choice(self.octave)
-        self.result.append(music21.note.Note(pitch+octave, quarterLength=duration))
-    
-    # Generates an appraoch tone (forces the next note to be a chord tone)
-    def generateApproachNote(self, chord, duration, curBeats):
-        try:
-            nextSymbol = self.sequence.pop(0)
-        except: # reached the end of the sequence (this is most likely impossible given the grammar)
-            # Since we've reached the end of the sequence, it makes sense for the last note to be a chord tone
-            self.generateChordColorScaleNote(chord, "C", duration)
-            return
-        
-        # Forcing the next note to be a chord tone
-        print(nextSymbol)
-        nextDuration = self.rhythmMap[nextSymbol[1]]
-        nextPitch = choice(self.chordTone[chord])
-        
-
-        pitch = choice(self.approachTone[chord][nextPitch])
-        octave = choice(self.octave)
-        self.result.append(music21.note.Note(pitch+octave, quarterLength=duration))
-
-        # Currently default the next chord tone to be in the same octave 
-        # TODO: adjust the octave according to the enclosure rules in the approach tone
-        self.result.append(music21.note.Note(nextPitch+octave, quarterLength=nextDuration))
-        curBeats += nextDuration        
+            
 
 
     # Maps the sequence into a stream of notes 
     def generate(self, filename):
-        curBeats = 0 # keeping track of the number of beats
-        for chord in self.chordSequence:
-            if curBeats > 4:
-                curBeats = curBeats - 4
-            elif curBeats == 4:
-                curBeats = 0
-            print("chord is : {}".format(chord))
-            while curBeats < 4:
-                try:
-                    curSymbol = self.sequence.pop(0) # this is because of the "striding across bars" #TODO: fix this issue
-                except:
-                    break
-                print(curSymbol)
-                pitch = curSymbol[0]
-                duration = self.rhythmMap[curSymbol[1]]
+        
 
-                # Appending the next note to the result
-                if pitch == "R": # Rest
-                    self.result.append(music21.note.Rest(quarterLength=duration))
-                elif pitch == "C" or pitch == "S" or pitch == "L":
-                    self.generateChordColorScaleNote(chord, pitch, duration)
-                elif pitch == "A": # approach tone (forces the next note to be a chord tone)
-                    self.generateApproachNote(chord, duration, curBeats)
-                elif pitch == "H": # one of chord tone, color tone or approach tone
-                    pitch = choice(["C", "L", "A"])
-                    if pitch == "A":
-                        self.generateApproachNote(chord, duration, curBeats)
-                    else:
-                        self.generateChordColorScaleNote(chord, pitch, duration)
-                elif pitch == "X": # could be any tone
-                    pitch = choice(["C", "L", "A", "S"])
-                    if pitch == "A":
-                        self.generateApproachNote(chord, duration, curBeats)
-                    else:
-                        self.generateChordColorScaleNote(chord,pitch,duration)
-                
-                # Updating the curBeats
-                curBeats += duration
+
+
                     
         
         
-        fp = self.result.write('midi', fp="./results/"+filename)
+        # fp = self.result.write('midi', fp="./results/"+filename)
+        return
     
 
 
